f44e5fa6bf5bfc3e5f1f798cd21a5507abeff433
juniorlimajr88-QA <juninho.limajr88@gmail.com>
2026-05-25 15:10:08 -0300

    Add Ramdon Values on ser spec chancejs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,6 +4,8 @@ import DashboardPage from '../pages/dashboardPage.js'
 import MenuPage from '../pages/menuPage.js'
 import InfoPage from '../pages/InfoPage.js'
 
+const Chance = require('chance');
+const chance = new Chance();
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
@@ -17,7 +19,7 @@ describe('Orange HRM Tests', () => {
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkDashboardPage()
     menuPage.accessMyInfo()
-    infoPage.fillPersonalDetails('firstName', 'middleName', 'lastName')
+    infoPage.fillPersonalDetails(chance.first(), 'middlename', chance.last())
     infoPage.fillEmployeeDetails('employeeId', 'Otherid', '123456', '1988-03-02')
     infoPage.saveForm()
 
